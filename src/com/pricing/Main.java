fe1b8785cc8e84c7d45a10de35deb0e48ccfc86d
Ihor Reshetnov <ihor.reshetnov@get.no>
2017-04-21 12:50:09 +0300

    Fix main to run.

--- src/com/pricing/Main.java
+++ src/com/pricing/Main.java
@@ -3,6 +3,7 @@ package com.pricing;
 public class Main {
 
     public static void main(String[] args) {
+        System.setProperty("is.production", "false");
         PriceCalculate
             priceCalculate =
             new PriceCalculate(new PriceCalculate.Price(20L), new PriceCalculate.Price(30L));
